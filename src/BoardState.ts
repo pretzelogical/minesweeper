82909b8b815a66cc638ab6c2e29b19ab45384c0d
pretzelogical <yingyangyo266@gmail.com>
2024-10-08 15:44:45 -0500

    Initialize board with bombs

--- src/BoardState.ts
+++ src/BoardState.ts
@@ -7,27 +7,58 @@ enum SpaceStates {
   Mined
 }
 
-type BoardSpace = {
+type BoardSpaceType = {
   state: SpaceStates,
   num: number
 }
 
-type BoardState = {
-  board: Array<Array<BoardSpace>>
+type BoardType = Array<Array<BoardSpaceType>>
+
+type MineSweeperState = {
+  board: BoardType,
+  initialize: (width: number, height: number, bombs: number) => void
 }
 
-type BoardActions = {
-  test: () => void
+type GameState = {
+  minesweeper: MineSweeperState
 }
 
-export const useBoardStore = create<BoardState & BoardActions>()(
+
+export const useBoardStore = create<GameState>()(
   immer((set) => ({
-    board: [
-      [{ state: SpaceStates.Empty, num: 0 }, { state: SpaceStates.Empty, num: 0 }, { state: SpaceStates.Empty, num: 0 }, { state: SpaceStates.Empty, num: 0 }],
-      [{ state: SpaceStates.Empty, num: 0 }, { state: SpaceStates.Empty, num: 0 }, { state: SpaceStates.Empty, num: 0 }, { state: SpaceStates.Empty, num: 0 }],
-      [{ state: SpaceStates.Empty, num: 0 }, { state: SpaceStates.Empty, num: 0 }, { state: SpaceStates.Empty, num: 0 }, { state: SpaceStates.Empty, num: 0 }],
-      [{ state: SpaceStates.Empty, num: 0 }, { state: SpaceStates.Empty, num: 0 }, { state: SpaceStates.Empty, num: 0 }, { state: SpaceStates.Empty, num: 0 }],
-    ] as Array<Array<BoardSpace>>,
-    test: () => { void (0); }
+    minesweeper: {
+      board: [
+        [{ state: SpaceStates.Empty, num: 0 }, { state: SpaceStates.Empty, num: 0 }, { state: SpaceStates.Empty, num: 0 }, { state: SpaceStates.Empty, num: 0 }],
+        [{ state: SpaceStates.Empty, num: 0 }, { state: SpaceStates.Empty, num: 0 }, { state: SpaceStates.Empty, num: 0 }, { state: SpaceStates.Empty, num: 0 }],
+        [{ state: SpaceStates.Empty, num: 0 }, { state: SpaceStates.Empty, num: 0 }, { state: SpaceStates.Empty, num: 0 }, { state: SpaceStates.Empty, num: 0 }],
+        [{ state: SpaceStates.Empty, num: 0 }, { state: SpaceStates.Empty, num: 0 }, { state: SpaceStates.Empty, num: 0 }, { state: SpaceStates.Empty, num: 0 }],
+      ] as BoardType,
+      initialize: (width: number, height: number, bombs: number): void => {
+        const newBoard: BoardType = [];
+        for (let i = 0; i < height - 1; i++) {
+          let row = [];
+          for (let j = 0; j < width - 1; j++) {
+            row.push({ state: SpaceStates.Empty, num: 0 });
+          }
+          newBoard.push(row);
+        }
+
+        let bombsRemaining = bombs;
+
+        while (bombsRemaining > 0) {
+          let bx = Math.floor(Math.random() * (width - 1));
+          let by = Math.floor(Math.random() * (height - 1));
+
+          if (newBoard[by][bx].state === SpaceStates.Mined) {
+            continue;
+          }
+          newBoard[by][bx].state = SpaceStates.Mined;
+          bombsRemaining -= 1;
+        }
+        set((state) => {
+          state.minesweeper.board = newBoard;
+        })
+      }
+    }
   }))
 );
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,13 +1,18 @@
+import { useEffect } from "react"
 import { useBoardStore } from "../BoardState"
 
 
 export default function Board() {
-  const board = useBoardStore((store) => store.board)
+  const board = useBoardStore((store) => store.minesweeper.board)
+  const initializeBoard = useBoardStore((store) => store.minesweeper.initialize)
+  useEffect(() => {
+    initializeBoard(4, 6, 3);
+  }, [])
 
   const spaces = board.map((row) => (
     <div className="flex">
       {row.map((space) => (
-        <p>{space.num}</p>
+        <p>{space.state}</p>
       ))}
     </div>
   ))
