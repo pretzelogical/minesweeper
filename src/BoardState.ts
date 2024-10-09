f8a043d187d31c905627acb1eb44d72f5e27aa8a
pretzelogical <yingyangyo266@gmail.com>
2024-10-08 22:33:23 -0500

    Add mine detection numbers

--- src/BoardState.ts
+++ src/BoardState.ts
@@ -19,7 +19,8 @@ type BoardType = Array<Array<BoardSpaceType>>
 
 type MineSweeperState = {
   board: BoardType,
-  initialize: (width: number, height: number, bombs: number) => void
+  initialize: (width: number, height: number, bombs: number) => void,
+  click: (x: number, y: number) => void
 }
 
 type GameState = {
@@ -49,12 +50,13 @@ export const useBoardStore = create<GameState>()(
       board: [
         [newSpace({})]
       ] as BoardType,
+
       initialize: (width: number, height: number, bombs: number): void => {
         const newBoard: BoardType = [];
-        for (let i = 0; i < height - 1; i++) {
+        for (let by = 0; by <= height; by++) {
           let row = [];
-          for (let j = 0; j < width - 1; j++) {
-            row.push(newSpace({ id: `x: ${j}, y: ${i}`, pos: { x: j, y: i } }));
+          for (let bx = 0; bx <= width; bx++) {
+            row.push(newSpace({ id: `x: ${bx}, y: ${by}`, pos: { x: bx, y: by } }));
           }
           newBoard.push(row);
         }
@@ -71,9 +73,48 @@ export const useBoardStore = create<GameState>()(
           newBoard[by][bx].state = SpaceStates.Mined;
           bombsRemaining -= 1;
         }
+
+        const getMineNum = (bx: number, by: number) => {
+          if (newBoard[by][bx].state === SpaceStates.Mined) {
+            return 0;
+          }
+          let mineNum = 0;
+
+          // Top row
+          for (let oy = -1; oy <= 1; oy++) {
+            for (let ox = -1; ox <= 1; ox += 1) {
+              if (
+                by + oy < 0
+                || bx + ox < 0
+                || by + oy > newBoard.length - 1
+                || bx + ox > newBoard[0].length - 1
+              ) {
+                continue;
+              }
+              if (newBoard[by + oy][bx + ox].state === SpaceStates.Mined) {
+                mineNum += 1;
+              }
+            }
+          }
+
+          return mineNum;
+        }
+
+        for (let by = 0; by <= height; by++) {
+          for (let bx = 0; bx <= width; bx++) {
+            newBoard[by][bx].num = getMineNum(bx, by);
+          }
+        }
         set((state) => {
           state.minesweeper.board = newBoard;
         })
+      },
+
+      click: (x: number, y: number) => {
+        // set((state) => {
+        //   state.board = checkBoard(x, y)
+        // })
+        void 0;
       }
     }
   }))
